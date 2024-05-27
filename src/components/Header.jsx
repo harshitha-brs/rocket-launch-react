dc0f1a8084efc85fa5cf27cc925e75558c053149
harshitha-brs <153584944+harshitha-brs@users.noreply.github.com>
2024-05-27 11:54:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
               </div>
             </div>
           </div>
-          <div><h1>JS Rocket Launch!</h1></div>
+          
         </div>
       </header>
   );
